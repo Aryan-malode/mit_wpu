a0a94a3dd6f7f2db6b55136f29c2ee231e4c976e
Aryan-malode <163159252+Aryan-malode@users.noreply.github.com>
2024-03-12 21:34:35 +0530

    yay

---
+++ java/exp1/1a.java
@@ -0,0 +1,9 @@
+
+public class hlo_wrd {
+
+    public static void main(String[] args) {
+       float sci =35e4f;
+        System.out.println("Hello world");
+        System.out.println(sci);
+    }
+}
